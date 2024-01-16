5354ed21af23b8143e5b3beeef608db9be25df1c
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-01-16 20:03:30 +0200

    add content to the landing

--- script/main.js
+++ script/main.js
@@ -6,3 +6,16 @@ icon.addEventListener('click', () => {
   links.classList.toggle('open');
 });
 
+// Handle The Sliders
+const slider1 = new Swiper('.slider-one', {
+  speed: 1500,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-one-next',
+    prevEl: '.swiper-one-prev',
+  },
+});
